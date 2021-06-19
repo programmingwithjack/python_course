e9e7e896eb4bcec72cb690f6d7ba0ca2e0cbfe13
programmingwithjack <86165809+programmingwithjack@users.noreply.github.com>
2021-06-20 01:16:08 +0530

    Create string1.py

---
+++ string1.py
@@ -0,0 +1,19 @@
+## string 
+print("Hello")
+print('hello')
+a="hello"
+print(a)
+b='hello'
+print(b)
+#### multiple line string
+c='''python is a awesome language
+its very easy and user friendly'''
+# string is like a array
+a = "Hello, World!"
+print(a[1])
+# string slicing
+b = "Hello, World!"
+print(b[2:5])
+# negative indexing
+b = "Hello, World!"
+print(b[-5:-2])
