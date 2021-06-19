f9110c33aa64acca4ddc8debb953f6c7d5b8449e
programmingwithjack <86165809+programmingwithjack@users.noreply.github.com>
2021-06-20 01:05:34 +0530

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,26 @@
+# variable  python
+a=5     # int
+b="hello"    #string
+print(a)
+print(b)
+# assign multiple variable
+x,y,z="hello","world","good"
+print(x)
+print(y)
+print(z)
+# output variable
+x = "awesome"
+print("Python is " + x)
+x = "Python is "
+y = "awesome"
+z =  x + y
+print(z)
+x = 5
+y = 10
+print(x + y)
+# global varibale
+# when function completed than explain global variable
+
+
+
+
