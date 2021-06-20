c3f13078b56f58647a82918cffd68c933b16e47c
programmingwithjack <86165809+programmingwithjack@users.noreply.github.com>
2021-06-20 16:04:44 +0530

    Create modify_string.py

---
+++ modify_string.py
@@ -0,0 +1,20 @@
+# upper
+a = "Hello, World!"
+print(a.upper())
+#lower
+a = "Hello, World!"
+print(a.lower())
+#remove white space
+a = "Hello, World!"
+print(a.strip())
+# replace
+a = "Hello, World!"
+print(a.replace("H", "J"))
+# split
+a = "Hello, World!"
+print(a.split(",")) # returns ['Hello', ' World!']
+#concatenate
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
